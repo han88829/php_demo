dc66db77ea6d9adb2eaf88f27f3e9230ba8ebe4d
Han <hanbeibei@joybroad.com>
2018-08-14 14:44:34 +0800

    Add        index.html Add        index.php

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php 
+//   phpinfo();
+// $name = '我是php，';
+// $age  = "$name 年龄\"21";
+
+// 浮点数
+// $num = 123.8273;
+// echo $num;
+// $number = 0.8813;
+// var_dump($number);
+
+// $isNaN = null;
+
+// if(empty($isNaN))
+// echo '我是false或者null';
+// else 
+// echo '为真';
+
+// $one = 10;
+// $two = false;
+// $three = 0;
+// $four = null;
+// $data = array(
+//     'num'=>123,
+//     'arr'=>array(
+//         'num'=>222,
+//     ),
+// );
+
+// $result = isset($one , $two , $three , $four);
+// // isset只检查是否有null，有一个就返回true
+// //执行看看结果，是不是
+// // unset 删除变量中的指定字段
+// unset($data['num']);
+// var_dump(gettype($data));
+
+
+// $shu = 'biao';
+// $biao = 'wo';
+// $wo = 'test';
+// $test = 'sina';
+// $sina = 'zhongguo';
+// $zhongguo = 'china';
+// $china = '我爱你';
+// //别运行，自己去推理一下代码。也写几个可变变量玩玩吧！
+// echo $$$$$shu;
+
+$data  = $_REQUEST['data'];
+// echo $data;
+
+print_r(123);
+
+?>
